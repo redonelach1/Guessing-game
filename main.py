73775320e1309ed30dd89b9a2b314cb4379fba53
Redone Lachgar <rednahdawi@gmail.com>
2022-03-04 17:13:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
 def guess(x):
-  a =1
-  b=x
+  a = 1
+  b = x
   randomN = random.randint(a,b)
   # print(randomN)
-  guess=int(input("your guess:"))
-  while(guess!=randomN):
+  guess = int(input("your guess:"))
+  while (guess!=randomN):
     if (guess > randomN):
       print("Too high")
     elif(guess < randomN):
@@ -13,9 +13,9 @@ def guess(x):
     guess = int(input("your guess:"))
   print("success!")
 def computerguess(x):
-  low=1
-  high=x
-  feedback=''
+  low = 1
+  high = x
+  feedback = ''
   guess = 0
   while(feedback!='c'):
     guess = random.randint(low, high)
@@ -25,11 +25,11 @@ def computerguess(x):
     elif(feedback=='l'):
       low = guess+1
 
-  print(f"Gotcha your number is {guess}")
-choice=int(input("Which One do you want to guess? : \n1.You\n2.The Computer\n:"))
+  print(f"your number is {guess}")
+choice = int(input("Which One do you want to guess? : \n1.You\n2.The Computer\n:"))
 if (choice == 1):
-      x=int(input("guessing range: "))
+      x = int(input("guessing range: "))
       guess(x)
 if (choice == 2):
-      x=int(input("guessing range: "))
+      x = int(input("guessing range: "))
       computerguess(x)
